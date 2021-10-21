68e977d40bf65b9a1ee1562b9a8d0edd945f24b4
LightDDark <myavraham25@gmail.com>
2021-10-21 20:05:54 +0300

    Added Header file and main Tester

--- anomaly_detection_util.cpp
+++ anomaly_detection_util.cpp
@@ -1,49 +1,88 @@
-#include "anomaly_detection_util.h"
-#include <cmath>
+/*
+ * animaly_detection_util.cpp
+ *
+ * Author: write your ID and name here
+ */
 
+#include <math.h>
+#include "anomaly_detection_util.h"
 float* copy(float* x, int size);
 float sum(float* x, int size);
-float sqr(float x, int power);
-float sqrArr(float* x, int size, int power);
+float pow(float x, int power);
+float powArr(float* x, int size, int power);
 
-float var(float* x, int size) {
-    float* newX = copy(x, size);
-    float u = (1 / size) * sum(newX, size);
-    sqrArr(newX, size, 2);
-    float var = ((1 / size) * sum(newX, size)) - sqr(u, 2);
-    delete[] newX;
-    return var;
+float avg(float* x, int size){
+	return 0;
+}
+
+// returns the variance of X and Y
+float var(float* x, int size){
+	float* newX = copy(x, size);
+	float u = (1 / size) * sum(newX, size);
+	powArr(newX, size, 2);
+	float var = ((1 / size) * sum(newX, size)) - sqr(u, 2);
+	delete[] newX;
+	return var;
+}
+
+// returns the covariance of X and Y
+float cov(float* x, float* y, int size){
+	return 0;
+}
+
+
+// returns the Pearson correlation coefficient of X and Y
+float pearson(float* x, float* y, int size){
+	return 0;
+}
+
+// performs a linear regression and returns the line equation
+Line linear_reg(Point** points, int size){
+
+	return Line(0,0);
+}
+
+// returns the deviation between point p and the line equation of the points
+float dev(Point p,Point** points, int size){
+	return 0;
+}
+
+// returns the deviation between point p and the line
+float dev(Point p,Line l){
+	return 0;
 }
 
 float* copy(float* x, int size) {
-    float* newX = new float[size];
-    for (int i = 0; i < size; i++)
-    {
-        *(newX + i) = *(x + i);
-    }
-    return newX;
+	float* newX = new float[size];
+	for (int i = 0; i < size; i++)
+	{
+		*(newX + i) = *(x + i);
+	}
+	return newX;
 }
 
 float sum(float* x, int size) {
-    float sum = 0;
-    for (int i = 0; i < size; i++)
-    {
-       sum += *(x + i);
-    }
-    return sum;
+	float sum = 0;
+	for (int i = 0; i < size; i++)
+	{
+		sum += *(x + i);
+	}
+	return sum;
 }
 
-float sqr(float x, int power) {
-    for (int i = 0; i < power; i++)
-    {
-        x *= x;
-    }
-    return x;
+float pow(float x, int power) {
+	for (int i = 0; i < power; i++)
+	{
+		x *= x;
+	}
+	return x;
 }
 
-float sqrArr(float* x, int size, int power) {
-    for (int i = 0; i < size; i++)
-    {
-        *(x + i) = sqr(*(x + i), power);
-    }
+float powArr(float* x, int size, int power) {
+	for (int i = 0; i < size; i++)
+	{
+		*(x + i) = pow(*(x + i), power);
+	}
 }
+
+
