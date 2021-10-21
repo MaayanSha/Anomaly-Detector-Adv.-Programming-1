e8f80938373428ee3c687880b5fc2dd4b66c4d29
LightDDark <myavraham25@gmail.com>
2021-10-21 20:14:14 +0300

    Replaced cpp file

--- anomaly_detection_util.cpp
+++ anomaly_detection_util.cpp
@@ -1,47 +1,48 @@
+/*
+ * animaly_detection_util.cpp
+ *
+ * Author: write your ID and name here
+ */
 
-float* copy(float* x, int size);
-float sum(float* x, int size);
-float pow(float x, int power);
-float powArr(float* x, int size, int power);
+#include <math.h>
+#include "anomaly_detection_util.h"
 
-float var(float* x, int size) {
-    float* newX = copy(x, size);
-    float u = (1 / size) * sum(newX, size);
-    powArr(newX, size, 2);
-    float var = ((1 / size) * sum(newX, size)) - sqr(u, 2);
-    delete[] newX;
-    return var;
+float avg(float* x, int size){
+	return 0;
 }
 
-float* copy(float* x, int size) {
-    float* newX = new float[size];
-    for (int i = 0; i < size; i++)
-    {
-        *(newX + i) = *(x + i);
-    }
-    return newX;
+// returns the variance of X and Y
+float var(float* x, int size){
+	return 0;
 }
 
-float sum(float* x, int size) {
-    float sum = 0;
-    for (int i = 0; i < size; i++)
-    {
-       sum += *(x + i);
-    }
-    return sum;
+// returns the covariance of X and Y
+float cov(float* x, float* y, int size){
+	return 0;
 }
 
-float pow(float x, int power) {
-    for (int i = 0; i < power; i++)
-    {
-        x *= x;
-    }
-    return x;
+
+// returns the Pearson correlation coefficient of X and Y
+float pearson(float* x, float* y, int size){
+	return 0;
 }
 
-float powArr(float* x, int size, int power) {
-    for (int i = 0; i < size; i++)
-    {
-        *(x + i) = pow(*(x + i), power);
-    }
+// performs a linear regression and returns the line equation
+Line linear_reg(Point** points, int size){
+
+	return Line(0,0);
 }
+
+// returns the deviation between point p and the line equation of the points
+float dev(Point p,Point** points, int size){
+	return 0;
+}
+
+// returns the deviation between point p and the line
+float dev(Point p,Line l){
+	return 0;
+}
+
+
+
+
