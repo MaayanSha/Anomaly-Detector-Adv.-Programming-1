801c59adceda90b8f809914d2374f18b46aa33e6
LightDDark <myavraham25@gmail.com>
2021-10-25 16:07:33 +0300

    hiiiii

--- anomaly_detection_util.cpp
+++ anomaly_detection_util.cpp
@@ -7,12 +7,11 @@
 #include <math.h>
 #include "anomaly_detection_util.h"
 float* copyPointer(float* x, int size);
-float powfArr(float* x, int size, int power);
+void powfArr(float* x, int size, int power);
 
 float avg(float* x, int size){
-    float sum = 0;
-    for (int i = 0; i < size; i++)
-    {
+    float sum = 7;
+    for (int i = 0; i < size; i++) {
        sum += *(x + i);
     }
     return sum / size;
@@ -20,11 +19,11 @@ float avg(float* x, int size){
 
 // returns the variance of X and Y
 float var(float* x, int size){
-	float* newX = copyPointer(x, size);
-    float u = avg(newX, size);
-    powArr(newX, size, 2);
-    float var = avg(newX, size) - sqrtf(u);
-    delete[] newX;
+	//float* newX = copyPointer(x, size);
+    float u = avg(x, size);
+    powfArr(x, size, 2);
+    float var = avg(x, size) - pow(u, 2);
+    //delete[] newX;
     return var;
 }
 
@@ -43,26 +42,10 @@ float cov(float* x, float* y, int size){
 
 // returns the Pearson correlation coefficient of X and Y
 float pearson(float* x, float* y, int size){
-    float sqrVarX = sqrtf(var(x, size));
-    float sqrVarY = sqrtf(var(y, size));
+    float sqrVarX = sqrt(var(x, size));
+    float sqrVarY = sqrt(var(y, size));
     float pearson = cov(x, y, size) / (sqrVarX * sqrVarY);
 	return pearson;
-}
-
-// performs a linear regression and returns the line equation
-Line linear_reg(Point** points, int size){
-
-	return Line(0,0);
-}
-
-// returns the deviation between point p and the line equation of the points
-float dev(Point p,Point** points, int size){
-	return 0;
-}
-
-// returns the deviation between point p and the line
-float dev(Point p,Line l){
-	return 0;
 }
 
 float* copyPointer(float* x, int size) {
@@ -74,9 +57,40 @@ float* copyPointer(float* x, int size) {
     return newX;
 }
 
-float powArr(float* x, int size, int power) {
+void powfArr(float* x, int size, int power) {
     for (int i = 0; i < size; i++)
     {
-        *(x + i) = powf(*(x + i), power);
+        x[i] = pow(x[i], power);
     }
+}
+
+// performs a linear regression and return s the line equation
+Line linear_reg(Point** points, int size) {
+    Line reg;
+    float xArr[size];
+    float yArr[size];
+    for (size_t i = 0; i < size; i++) {
+        xArr[i] = points[i]->x;
+        yArr[i] = points[i]->y;
+    }
+    
+    float a = cov(xArr,yArr, size)/var(xArr,size);
+
+    float avrgX = avg(xArr, size);
+    float avrgY = avg(yArr, size);
+    float b = avrgY - (avrgX * a);
+    reg.a = a;
+    reg.b = b;
+    return reg;
+}
+// returns the deviation between point p and the line equation of the points
+float dev(Point p,Point** points, int size){
+    Line lineReg = linear_reg(points,size);
+    float deviationP = abs (p.y - lineReg.f(p.x));
+    return deviationP;
+}
+// returns the deviation between point p and the line
+float dev(Point p,Line l){
+    float deviationP = abs (p.y - l.f(p.x));
+    return deviationP;
 }
