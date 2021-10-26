c7b68de2c1f4e5dbb3b5423b3750609822e64a22
LightDDark <77627113+LightDDark@users.noreply.github.com>
2021-10-26 13:29:36 +0300

    Added notes

--- anomaly_detection_util.cpp
+++ anomaly_detection_util.cpp
@@ -1,7 +1,3 @@
-//
-// Created by Maayan Shavit on 24/10/2021.
-//
-
 /*
  * animaly_detection_util.cpp
  *
@@ -11,62 +7,105 @@
 #include <math.h>
 #include "anomaly_detection_util.h"
 
+// Declares assisting functions
 float* copyPointer(float* x, int size);
 void powfArr(float* x, int size, int power);
 
+/*
+* Function avg reiceve a float array x
+* and returns the average of the array
+* by summin x values and dividing them by given size
+*/
 float avg(float* x, int size){
+    // delcare variable for summin x values
     float sum = 0;
+    // sum all values
     for (int i = 0; i < size; i++) {
         sum += *(x + i);
     }
+    // returns sum divided by array size
     return (sum / size);
 }
 
-// returns the variance of X and Y
+/*
+* functions returns variance of x values (inside given array x),
+* suing the formula Var(X) = avg(x^2) - avg(x)^2
+* note that capital letters denotes Array/Pointer and small letters denotes values
+*/
 float var(float* x, int size){
+    // copy x values to a new array (dynamic), in order to not affect x with further manipulations. 
     float* newX = copyPointer(x, size);
+    // calculate average
     float u = avg(newX, size);
+    // square all x values inside new array
     powfArr(newX, size, 2);
+    // calculate according to formula
     float var = avg(newX, size) - pow(u, 2);
+    // delete allocated memory
     delete[] newX;
     return var;
 }
 
-// returns the covariance of X and Y
+/*
+* returns the covariance of X and Y,
+* by using the formula cov(X,Y) = avg(x-avg(X)(y-avg(Y))
+* note that capital letters denotes Array/Pointer and small letters denotes values.
+*/
 float cov(float* x, float* y, int size){
+    // calculates average of each X and Y
     float avgX = avg(x, size);
     float avgY = avg(y, size);
+    // create a new Array for the multipcation of differance.
     float covArr[size];
     for (size_t i = 0; i < size; i++) {
+        // multiply the diff from avg in each array
         covArr[i] = (x[i] - avgX) * (y[i] - avgY);
     }
+    // return the average of the results
     return avg(covArr, size);
 }
 
 
-// returns the Pearson correlation coefficient of X and Y
+/*
+* returns the Pearson correlation coefficient of X and Y,
+* according to the formula cov(X,Y)/(sqr(var(x))*sqr(var(y)))
+*/
 float pearson(float* x, float* y, int size){
+    // calculate the square root of var for each array
     float sqrVarX = sqrt(var(x, size));
     float sqrVarY = sqrt(var(y, size));
+    // calculate according to formula
     float pearson = cov(x, y, size) / (sqrVarX * sqrVarY);
+    // returns pearson
     return pearson;
 }
 
+/*
+* function copyPointer, copies the content of float pointer x,
+* given it's size into a new allocated memory.
+*/
 float* copyPointer(float* x, int size) {
+    // declares pointer to float at the SIZE of size
     float* newX = new float[size];
-    for (int i = 0; i < size; i++)
-    {
+    // loops on x and copy values the new pointer
+    for (int i = 0; i < size; i++) {
         newX[i] = x[i];
     }
+    // returns new pinter
     return newX;
 }
 
+/*
+* function recieves an float array, size, and power to raise with,
+* and performs power on every x value inside array.
+*/
 void powfArr(float* x, int size, int power) {
-    for (int i = 0; i < size; i++)
-    {
+    // loops on the array and power each value
+    for (int i = 0; i < size; i++) {
         x[i] = pow(x[i], power);
     }
 }
+
 /*
     this function claculates the linear regression
     for an array of points. It returns the line equation
@@ -98,6 +137,7 @@ Line linear_reg(Point** points, int size) {
     reg.b = b;
     return reg;
 }
+
 /*
     this function claculates the deviation point
     between a given point and the rest of the points
@@ -107,6 +147,7 @@ float dev(Point p,Point** points, int size){
     float deviationP = abs (p.y - lineReg.f(p.x));
     return deviationP;
 }
+
 /*
     this function claculates the deviation point 
     between a given point and a given linear regression line.
